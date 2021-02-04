6ecb964d100e351cb7dfc991fad50cc79aa2b90d
nakagawa.makoto <wwwww.desire@gmail.com>
2021-02-04 21:13:04 +0900

    TypeScriptでIE11変換が効くように試行錯誤中

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// import 'core-js'
+import 'core-js/es6/array'
+
 window.addEventListener('load', () => {
   alert(Array.from('InternetExplorer'))
 })
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,25 @@
+const path = require('path')
+
 module.exports = {
+  mode: 'production',
+  target: ['web', 'es5'],
+  entry: {
+    index: path.join(__dirname, '/src/index.ts')
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'main.js'
+  },
   module: {
     rules: [
+      {
+        test: /\.ts$/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      },
       {
         test: /\.js$/,
         use: [
@@ -10,5 +29,8 @@ module.exports = {
         ]
       }
     ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
   }
 }
