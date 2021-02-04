464aeb7ffa1bad27d65470041d0aa82baa4b9193
nakagawa.makoto <wwwww.desire@gmail.com>
2021-02-04 20:40:18 +0900

    導入, IE11用のbabel+webpack, polyfill

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ]
+      }
+    ]
+  }
+}
